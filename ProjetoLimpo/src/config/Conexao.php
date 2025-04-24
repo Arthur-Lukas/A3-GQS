fd4ebba282dd5d990e98030b95572fd8f62b366d
Arthur-Lukas <evangelistamachado01@gmail.com>
2025-04-24 19:11:54 -0300

    Começo do código limpo

---
+++ ProjetoLimpo/src/config/Conexao.php
@@ -0,0 +1,16 @@
+<?php
+class Conexao {
+    private static $host = 'localhost';
+    private static $dbname = 'db_biblioteca';
+    private static $user = 'root';
+    private static $password = '';
+
+    public static function conectar() {
+        try {
+            return new PDO("mysql:host=" . self::$host . ";dbname=" . self::$dbname, self::$user, self::$password);
+        } catch (PDOException $e) {
+            die("Erro na conexão: " . $e->getMessage());
+        }
+    }
+}
+?>
